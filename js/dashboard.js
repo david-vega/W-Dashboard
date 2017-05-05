3a9cf7e2ca9593963981a3d2d9dc4a03c188686c
David Vega <me@david-vega.com>
2017-05-05 10:45:39 -0700

    prepare for async rendering

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,24 +1,23 @@
 $(function() {
-  var environments = ['production', 'alpha', 'beta', 'preprod']
-  var services = ['bizshield-service', 'email-service']
-  var columns = 12
-  var columnsSize = columns / environments.length
+  var environments = ['production', 'alpha', 'beta', 'preprod'];
+  var services = ['bizshield-service', 'email-service'];
+  var columns = 12;
+  var columnsSize = columns / environments.length;
 
   services.forEach(function(service){
     $('body').append('<div class="mdl-grid" id="'+ service +'"></div>')
     environments.forEach(function(environment) {
-      var htmlTemplate = showInformation(service, environment, columnsSize)
+      var htmlTemplate = showInformation(service, environment, columnsSize);
       $('#'+ service).append(htmlTemplate);
 
       renderHealthCheck(service, environment);
+      renderGitInformation(service, environment);
+      renderContainerInformation(service, environment);
     });
   });
 });
 
 function showInformation(service, environment, columnsSize){
-  var githubInformation = fetchGithubInformation(service, environment);
-  var containerInformation = fetchContainerInformation(service, environment);
-
   return `
     <div class="mdl-card mdl-cell mdl-cell--`+ columnsSize +`-col" id="`+ service +'-'+ environment +`">
       <div class="mdl-card__title">
@@ -27,17 +26,15 @@ function showInformation(service, environment, columnsSize){
       </div>
       <div class="github-information">
         <div class="mdl-card__supporting-text">
-          <img alt="`+ githubInformation.userName +`" class="avatar" src="`+ githubInformation.photoSource +`">
-          <ul class="github-information mdl-list">
-            <li class="mdl-list__item">
+          <img alt="" class="avatar" src="">
+          <ul class="mdl-list">
+            <li class="mdl-list__item email">
               <span class="mdl-list__item-primary-content">
-                `+ githubInformation.userName+`
               </span>
             </li>
-            <li class="mdl-list__item">
+            <li class="mdl-list__item git-sha">
               <span class="mdl-list__item-primary-content">
-                <a class="commit-tease-sha" href="https://github.com/OtoAnalytics/`+ service +`/commit/`+ githubInformation.revisionNumber +`" target="blank">
-                  `+ githubInformation.revisionNumber.substring(0, 7) +`
+                <a class="commit-tease-sha" href="" target="blank">
                 </a>
               </span>
             </li>
@@ -45,6 +42,26 @@ function showInformation(service, environment, columnsSize){
         </div>
       </div>
       <div class="mdl-grid container-information">
+        <div class="mdl-card__supporting-text">
+          <ul class="mdl-list">
+            <li class="mdl-list__item cpu">
+              <span class="mdl-list__item-primary-content">
+              </span>
+            </li>
+            <li class="mdl-list__item memory">
+              <span class="mdl-list__item-primary-content">
+              </span>
+            </li>
+            <li class="mdl-list__item instances">
+              <span class="mdl-list__item-primary-content">
+              </span>
+            </li>
+            <li class="mdl-list__item disk">
+              <span class="mdl-list__item-primary-content">
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   `;
@@ -58,35 +75,56 @@ function renderHealthCheck(service, environment){
     dataType: 'jsonp',
     contentType: 'application/json',
     success: function (data) {
-      console.log('success')
-      var status = data.status || 'Something went wrong'
-      handleStatusResponse(status, service, environment)
+      var status = data.status || 'Something went wrong';
+      handleStatusResponse(status, service, environment);
     },
     error: function (data) {
-      console.log('error')
-      var status = data.status || 'Something went wrong'
-      handleStatusResponse(status, service, environment)
+      var status = data.status || 'Something went wrong';
+      handleStatusResponse(status, service, environment);
     }
   });
 };
 
 function handleStatusResponse(status, service, environment){
-  $('#'+ service +'-'+ environment +' h2').text("Status: "+ status)
+  $('#'+ service +'-'+ environment +' h2').text("Status: "+ status);
 };
 
-function fetchGithubInformation(service, environment){
-  var userName =  "@david-vega";
-  var photoSource = "https://avatars3.githubusercontent.com/u/1914931?v=3&amp;s=80"
-  var revisionNumber = "fe1ac36c1886e87d23079a89403822aea6d559a6"
+function renderGitInformation(service, environment){
+  var email =  "zetacu@gmail.com";
+  var revisionNumber = "035e8a8693851d276cba9e2e96939717c5549994";
 
-  return { userName: userName, photoSource: photoSource, revisionNumber: revisionNumber }
+  handleGitResponse(email, revisionNumber, service, environment);
 };
 
-function fetchContainerInformation(service, environment){
+function handleGitResponse(email, revisionNumber, service, environment){
+  var id = '#'+ service +'-'+ environment + ' .github-information';
+
+  $(id + ' img')
+    .attr('src', 'https://www.gravatar.com/avatar/'+ md5(email))
+    .attr('alt', email);
+
+  $(id + ' .email span').text(email);
+
+  var href = 'https://github.com/OtoAnalytics/'+ service +'/commit/'+ revisionNumber;
+  $(id + ' .git-sha a')
+    .text('ref: '+ revisionNumber.substring(0, 7))
+    .attr('href', href);
+};
+
+function renderContainerInformation(service, environment){
   var instances = 2;
   var cpu = 20;
   var memory = 30;
   var disk = 50;
 
-  return { instances: instances, memory: memory, cpu: cpu, disk: disk }
+  handleContainerResponse(instances, memory, cpu, disk, service, environment);
+};
+
+function handleContainerResponse(instances, memory, cpu, disk, service, environment){
+  var id = '#'+ service +'-'+ environment + ' .container-information';
+
+  $(id +' .cpu span').text('cpu: '+ cpu +'%')
+  $(id +' .memory span').text('memory: '+ memory +'%')
+  $(id +' .disk span').text('disk: '+ disk +'%')
+  $(id +' .instances span').text('instances: '+ instances)
 };
